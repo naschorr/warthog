c2466448ff62e7383ee7b378b693ce5624d2d1ba
Nick Schorr <naschorr@gmail.com>
2025-10-02 19:45:39 -0700

    Reversed tier ordering, as it was backwards originally

--- src/replay_data_explorer/configuration/graph_configuration.py
+++ src/replay_data_explorer/configuration/graph_configuration.py
@@ -3,11 +3,11 @@ from src.replay_data_explorer.common import hex_to_rgba
 
 # Define common colors for battle rating tiers
 BATTLE_RATING_TIER_COLORS = {
-    BattleRatingTier.DOWNTIER: {"hex": "#ff0000"},
-    BattleRatingTier.PARTIAL_DOWNTIER: {"hex": "#FF5A00"},
+    BattleRatingTier.DOWNTIER: {"hex": "#0A9200"},
+    BattleRatingTier.PARTIAL_DOWNTIER: {"hex": "#00C24E"},
     BattleRatingTier.BALANCED: {"hex": "#0000ff"},
-    BattleRatingTier.PARTIAL_UPTIER: {"hex": "#00C24E"},
-    BattleRatingTier.UPTIER: {"hex": "#0A9200"},
+    BattleRatingTier.PARTIAL_UPTIER: {"hex": "#FF5A00"},
+    BattleRatingTier.UPTIER: {"hex": "#ff0000"},
 }
 
 # Define Plotly specific configurations
@@ -20,26 +20,26 @@ PLOTLY_BATTLE_RATING_TIER_STATUS_COLORS = {
 }
 
 PLOTLY_BATTLE_RATING_TIER_STATUS_ORDER = [
-    BattleRatingTier.UPTIER,
-    BattleRatingTier.PARTIAL_UPTIER,
-    BattleRatingTier.BALANCED,
-    BattleRatingTier.PARTIAL_DOWNTIER,
     BattleRatingTier.DOWNTIER,
+    BattleRatingTier.PARTIAL_DOWNTIER,
+    BattleRatingTier.BALANCED,
+    BattleRatingTier.PARTIAL_UPTIER,
+    BattleRatingTier.UPTIER,
 ]
 
 PLOTLY_TRENDLINE_OPACITY = 0.5
 
 PLOTLY_COLOR_SCALE = [
     (0, BATTLE_RATING_TIER_COLORS[BattleRatingTier.BALANCED]["hex"]),
-    (1, BATTLE_RATING_TIER_COLORS[BattleRatingTier.PARTIAL_DOWNTIER]["hex"]),
+    (1, BATTLE_RATING_TIER_COLORS[BattleRatingTier.PARTIAL_UPTIER]["hex"]),
 ]
 
 PLOTLY_SINGLE_COLOR = hex_to_rgba(BATTLE_RATING_TIER_COLORS[BattleRatingTier.BALANCED]["hex"], 0.75)
 
 PLOTLY_CONCLUSION_COLORS = {
-    "good": BATTLE_RATING_TIER_COLORS[BattleRatingTier.UPTIER]["hex"],
+    "good": BATTLE_RATING_TIER_COLORS[BattleRatingTier.DOWNTIER]["hex"],
     "neutral": "#808080",
-    "bad": BATTLE_RATING_TIER_COLORS[BattleRatingTier.DOWNTIER]["hex"],
+    "bad": BATTLE_RATING_TIER_COLORS[BattleRatingTier.UPTIER]["hex"],
 }
 
 MINIMUM_ITEMS_FOR_PLOTTING = 10
--- src/replay_data_explorer/services/battle_rating_tier_classifier.py
+++ src/replay_data_explorer/services/battle_rating_tier_classifier.py
@@ -9,22 +9,22 @@ class BattleRatingTierClassifier:
         Get the battle rating tier for a player based on their battle rating and the match's battle rating.
 
         Tiers (for a 1.0 BR match):
-        - Downtier: BR == 0
-        - Partial Downtier: BR > 0 and BR < 0.4
+        - Uptier: BR == 0
+        - Partial Uptier: BR > 0 and BR < 0.4
         - Balanced: 0.4 >= BR <= 0.6
-        - Partial Uptier: BR > 0.6 and BR < 1.0
-        - Uptier: BR == 1.0
+        - Partial Downtier: BR > 0.6 and BR < 1.0
+        - Downtier: BR == 1.0
         """
         if delta >= 0:
-            return BattleRatingTier.UPTIER
+            return BattleRatingTier.DOWNTIER
         elif 0 > delta > -0.4:
-            return BattleRatingTier.PARTIAL_UPTIER
+            return BattleRatingTier.PARTIAL_DOWNTIER
         elif -0.4 >= delta >= -0.6:
             return BattleRatingTier.BALANCED
         elif -0.6 > delta > -1.0:
-            return BattleRatingTier.PARTIAL_DOWNTIER
+            return BattleRatingTier.PARTIAL_UPTIER
         else:  # delta <= -1.0
-            return BattleRatingTier.DOWNTIER
+            return BattleRatingTier.UPTIER
 
     @staticmethod
     def get_battle_rating_tier(player: Player, replay: Replay) -> BattleRatingTier:
@@ -32,11 +32,11 @@ class BattleRatingTierClassifier:
         Get the battle rating tier for a player based on their battle rating and the match's battle rating.
 
         Tiers (for a 1.0 BR match):
-        - Downtier: BR == 0
-        - Partial Downtier: BR > 0 and BR < 0.4
+        - Uptier: BR == 0
+        - Partial Uptier: BR > 0 and BR < 0.4
         - Balanced: 0.4 >= BR <= 0.6
-        - Partial Uptier: BR > 0.6 and BR < 1.0
-        - Uptier: BR == 1.0
+        - Partial Downtier: BR > 0.6 and BR < 1.0
+        - Downtier: BR == 1.0
         """
         delta = round(player.battle_rating - replay.battle_rating, 2)
         return BattleRatingTierClassifier.get_battle_rating_tier_from_delta(delta)
